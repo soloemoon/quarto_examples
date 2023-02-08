ad0030687ed065e7c82f835da2e69d2617c770f6
Solomon Moon <57546826+soloemoon@users.noreply.github.com>
2023-02-08 17:29:10 -0600

    Delete Output to Different Formats.sh

--- Parameterized Report/Output to Different Formats.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-cd "$(dirname "$0")"
-
-params=("html" "pdf")
-
-for n in "${params[@]}";
-do
-    quarto render '/Users/solomonmoon/Documents/Quarto Examples/Parameterized Report/Parameterized Report.qmd' --to "$n" 
-done
